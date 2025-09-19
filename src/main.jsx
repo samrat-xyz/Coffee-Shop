a268f5a9dadd58be8b0a31fc2c05383f7310b562
Asif-vai <mr3366533@gmail.com>
2025-09-19 18:07:33 +0600

    fix routing

--- src/main.jsx
+++ src/main.jsx
@@ -2,18 +2,20 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { BrowserRouter, Route, Routes } from 'react-router'
+import { HashRouter, Route, Routes } from 'react-router'
 import Menu from './pages/Menu.jsx';
 import About from './pages/About.jsx';
 import OrderOnline from './pages/OrederOnline.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <Routes>
-      <Route path='/' element={<App />} />
-      <Route path='/menu' element={<Menu />} />
-      <Route path='/about' element={<About />} />
-      <Route path='/orderonline' element={<OrderOnline />} />
-    </Routes>
-  </BrowserRouter>
+  <StrictMode>
+    <HashRouter>
+      <Routes>
+        <Route path='/' element={<App />} />
+        <Route path='/menu' element={<Menu />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/orderonline' element={<OrderOnline />} />
+      </Routes>
+    </HashRouter>
+  </StrictMode>
 )
